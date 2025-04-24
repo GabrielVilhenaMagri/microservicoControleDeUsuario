dc8bb97c6ec627102020a442c7e3aefb1ecec311
GabrielVilhenaMagri <gabriel.vilhenamagri@gmail.com>
2025-04-24 15:06:55 -0300

    Alterações finais

--- src/main/java/com/example/userservice/UserserviceApplication.java
+++ src/main/java/com/example/userservice/UserserviceApplication.java
@@ -1,5 +1,6 @@
 package com.example.userservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
--- src/main/java/com/example/userservice/security/util/JwtUtil.java
+++ src/main/java/com/example/userservice/security/util/JwtUtil.java
@@ -4,25 +4,37 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
 
+
+
 @Component
 public class JwtUtil {
 
-    private static final String SECRET_KEY = "secreta_secreta_secreta_secreta_secreta_secreta";
+    private final String secret;
+    private Key key;
 
-    private static final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
     public String generateToken(String email){
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(key,SignatureAlgorithm.HS256)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1h
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -48,11 +60,10 @@ public class JwtUtil {
     }
 
     public boolean isTokenValid(String token, String email){
-        return(extractEmail(token).equals(email) && !isTokenExpired(token));
+        return extractEmail(token).equals(email) && !isTokenExpired(token);
     }
 
     private boolean isTokenExpired(String token) {
-        return(extractExpiration(token).before(new Date()));
+        return extractExpiration(token).before(new Date());
     }
-
 }
